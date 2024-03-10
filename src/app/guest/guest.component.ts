00b6685b2b23cacd7a4feda9d168eba79ef17b6f
Adam Kiss <kisssandoradam@gmail.com>
2024-03-11 00:43:44 +0100

    added empty grids

--- src/app/guest/guest.component.ts
+++ src/app/guest/guest.component.ts
@@ -1,12 +1,53 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {GridOptions} from "ag-grid-community";
+import {Guest} from "./guest";
+import {AgGridAngular} from "ag-grid-angular";
 
 @Component({
   selector: 'app-guest',
   standalone: true,
-  imports: [],
+  imports: [
+    AgGridAngular
+  ],
   templateUrl: './guest.component.html',
   styleUrl: './guest.component.scss'
 })
 export class GuestComponent {
+
+  gridOptions: GridOptions<Guest> = {
+    // Row Data: The data to be displayed.
+    rowData: [
+      {
+        name: "",
+        preferredGame1: "",
+        preferredGame2: "",
+        preferredGame3: "",
+        playWith: "",
+        bringGuest: false,
+        guestName: "",
+        anyComments: "",
+        itemType: "",
+        path: ""
+      },
+    ],
+
+    // Column Definitions: Defines & controls grid columns.
+    columnDefs: [
+      {field: "name", headerName: "Name", pinned: 'left', editable: false},
+      {field: "preferredGame1", headerName: "Preferred Game 1", editable: false},
+      {field: "preferredGame2", headerName: "Preferred Game 2", editable: false},
+      {field: "preferredGame3", headerName: "Preferred Game 3", editable: false},
+      {field: "playWith", headerName: "Play With", editable: false},
+      {field: "bringGuest", headerName: "Bring guest?", editable: false},
+      {field: "guestName", headerName: "Guest Name", editable: false},
+      {field: "anyComments", headerName: "Any comments?", editable: false},
+      {field: "itemType", headerName: "Item Type", editable: false},
+      {field: "path", headerName: "Path", editable: false}
+    ],
+
+    autoSizeStrategy: {
+      type: 'fitGridWidth'
+    },
+  }
 
 }
--- src/app/host/host.component.ts
+++ src/app/host/host.component.ts
@@ -1,12 +1,59 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {GridOptions} from "ag-grid-community";
+import {Host} from "./host";
+import {AgGridAngular} from "ag-grid-angular";
 
 @Component({
   selector: 'app-host',
   standalone: true,
-  imports: [],
+  imports: [
+    AgGridAngular
+  ],
   templateUrl: './host.component.html',
   styleUrl: './host.component.scss'
 })
 export class HostComponent {
+
+  gridOptions: GridOptions<Host> = {
+    // Row Data: The data to be displayed.
+    rowData: [
+      {
+        name: "",
+        game: "",
+        complexity: "",
+        language: "",
+        numberOfPlayers: "",
+        anyComments: "",
+        estimatedDuration: "",
+        numberOfTables: 0,
+        coHost: "",
+        location: "",
+        owner: "",
+        itemType: "",
+        path: ""
+      },
+    ],
+
+    // Column Definitions: Defines & controls grid columns.
+    columnDefs: [
+      {field: "name", headerName: "Name", pinned: 'left', editable: false},
+      {field: "game", headerName: "Game", editable: false},
+      {field: "complexity", headerName: "Complexity", editable: false},
+      {field: "language", headerName: "Language", editable: false},
+      {field: "numberOfPlayers", headerName: "Number of players", editable: false},
+      {field: "anyComments", headerName: "Any comments?", editable: false},
+      {field: "estimatedDuration", headerName: "Estimated duration", editable: false},
+      {field: "numberOfTables", headerName: "Number of tables", editable: false},
+      {field: "coHost", headerName: "Co-Host", editable: false},
+      {field: "location", headerName: "Location", editable: false},
+      {field: "owner", headerName: "Owner", editable: false},
+      {field: "itemType", headerName: "Item Type", editable: false},
+      {field: "path", headerName: "Path", editable: false}
+    ],
+
+    autoSizeStrategy: {
+      type: 'fitGridWidth'
+    },
+  }
 
 }
